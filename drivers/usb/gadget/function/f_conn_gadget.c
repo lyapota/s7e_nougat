a528bcf2cf1dd6ff9893611694f5b45286dfa53c
lyapota <lyapic@ukr.net>
2017-01-18 00:13:29 +0200

    setup config

--- drivers/usb/gadget/function/f_conn_gadget.c
+++ drivers/usb/gadget/function/f_conn_gadget.c
@@ -58,7 +58,7 @@
 #include <asm/uaccess.h>
 
 /* platform specific definitions */
-#define __ANDROID__
+/* ex) #define __ANDROID__ */
 
 /* platform specific pre-processing */
 #define CONN_GADGET_SHORTNAME "android_ssusbcon"
