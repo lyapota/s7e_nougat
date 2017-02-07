ffc1699f230f348c5bc43af26f1d4d8d92672e38
lyapota <lyapic@ukr.net>
2017-02-07 12:49:37 +0200

    magisk fix update apk procedure

--- BUILD/kernel/clean.sh
+++ BUILD/kernel/clean.sh
@@ -15,34 +15,18 @@ sed -i /security.mdpp.mass/d /system/build.prop
 sed -i /ro.hardware.keystore/d /system/build.prop
 
 # Remove SU images
-cd /data
-
-rm -rf /data/app/com.topjohnwu.magisk-1
-rm -rf /data/app/com.topjohnwu.magisk-2
-rm -rf /data/app/com.topjohnwu.magisk-3
-rm -rf /data/app/me.phh.superuser-1
-rm -rf /data/app/me.phh.superuser-2
-rm -rf /data/app/me.phh.superuser-3
-rm -rf /data/Magisk.apk
+rm -rf /data/magisk.apk
 rm -rf /cache/magisk.log /cache/last_magisk.log /cache/magiskhide.log \
        /cache/magisk /cache/magisk_merge /cache/magisk_mount /cache/unblock 2>/dev/null
 
-rm -rf /data/app/eu.chainfire.supersu-1
-rm -rf /data/app/eu.chainfire.supersu-2
-rm -rf /data/app/eu.chainfire.supersu-3
-rm -f /data/SuperSU.apk
+rm -rf /data/SuperSU.apk
 
 if [ $PARAM == "3" ] || [ $PARAM == "1" ]; then
-  rm -rf /data/data/com.topjohnwu.magisk
-  rm -rf /data/data/me.phh.superuser
-
-  rm -rf /data/Magisk.apk
   rm -rf /data/magisk.img /data/magisk_merge.img /data/stock_boot.img \
          /data/busybox /data/magisk /data/custom_ramdisk_patch.sh 2>/dev/null
 fi
 
 if [ $PARAM == "2" ] || [ $PARAM == "1" ]; then
-  rm -rf /data/data/eu.chainfire.supersu
   rm -rf /data/su.img /data/stock_boot*.gz /data/supersu /supersu
 fi
 
